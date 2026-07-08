11cfcdd09e1c23ae0dadf3dd4d48f0ebdd8f5932
Abhishek Kuradia <Abhishekkuradia291@gmail.com>
2026-07-08 08:57:45 +0530

    mySql code connected to serverJs

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 const http = require('http')
-const mysql=require('mysql')
+const mysql = require('mysql')
+const { parse } = require('path')
 
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'admin',
+    database: 'webapp'
+})
+
+db.connect((err) => {
+    if (err) {
+        console.error('Something is not working in SQL', err)
+    }
+    else {
+        console.log('Sucessfully connected to mySQL DB')
+    }
+})
 
 const server = http.createServer((req, res) => {
     if (req.method == 'POST' && req.url == '/submit') {
@@ -13,9 +29,29 @@ const server = http.createServer((req, res) => {
 
         req.on('end', () => {
             const parsedData = new URLSearchParams(storage)
-            console.log(`Name is ${parsedData.get('uName')}`)
-            console.log(`Email is ${parsedData.get('uMail')}`)
-            console.log('complete data was', storage)
+            const name = parsedData.get('uName')
+            const email = parsedData.get('uMail')
+
+            console.log(`Received info for ${name} <${email}>`)
+
+            const queryText = `INSERT INTO users VALUES(${name},${email})`
+
+            db.query(queryText, (err, result) => {
+                if (err) {
+                    console.err('Failed during Database update:', err)
+                    res.writeHead(500, { 'content-Type': 'text/plain' })
+                    return res.end('Internal Server Error: Could not save data.')
+                }
+                else {
+                    console.log(`Sucessfully Added Username: ${result.insertId}`)
+                    res.writeHead(200, { 'content-Type': 'text/plain' })
+                    res.end(`
+                        <h2>Sucess!</h2>
+                        <p>Thank you, ${name} your email ${email} has been saved.</p>
+                        <a href="javascript:history.back()">Go Back</a>
+                        `)
+                }
+            })
 
             res.end('Data Received')
         })
